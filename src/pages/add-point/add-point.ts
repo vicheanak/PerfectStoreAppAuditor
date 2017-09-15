101cf41d46c5849e5a074588dada65042113ff12
Vicheanak <vicheanak@gmail.com>
2017-09-15 11:51:43 +0700

    2 million dolalrs project

--- src/pages/add-point/add-point.ts
+++ src/pages/add-point/add-point.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the AddPointPage page.
@@ -15,8 +16,62 @@ import { HomePage } from '../home/home';
    templateUrl: 'add-point.html',
  })
  export class AddPointPage {
+   isReadyToSave: boolean;
+   imgSrc1: String;
+   imgSrc2: String;
+   @ViewChild('imgSrc11') imgSrc11;
+   @ViewChild('imgSrc22') imgSrc22;
+   @ViewChild('fileInput') fileInput;
 
-   constructor(public navCtrl: NavController, public navParams: NavParams) {
+   constructor(public navCtrl: NavController, public navParams: NavParams, public camera: Camera) {
+     this.imgSrc1 = '../../assets/img/take-photo.png';
+     this.imgSrc2 = '../../assets/img/take-photo.png';
+   }
+
+
+   processWebImage(event) {
+     let reader = new FileReader();
+     reader.onload = (readerEvent) => {
+
+       let imageData = (readerEvent.target as any).result;
+       this.imgSrc1 = imageData;
+
+     };
+
+     reader.readAsDataURL(event.target.files[0]);
+   }
+
+
+   getPicture1() {
+     if (Camera['installed']()) {
+       this.camera.getPicture({
+         destinationType: this.camera.DestinationType.DATA_URL,
+         targetWidth: 96,
+         targetHeight: 96
+       }).then((data) => {
+         this.imgSrc1 = 'data:image/jpg;base64,' + data;
+       }, (err) => {
+         alert('Unable to take photo');
+       })
+     } else {
+       this.fileInput.nativeElement.click();
+     }
+   }
+
+   getPicture2() {
+     if (Camera['installed']()) {
+       this.camera.getPicture({
+         destinationType: this.camera.DestinationType.DATA_URL,
+         targetWidth: 96,
+         targetHeight: 96
+       }).then((data) => {
+         this.imgSrc2 = 'data:image/jpg;base64,' + data;
+       }, (err) => {
+         alert('Unable to take photo');
+       })
+     } else {
+       this.fileInput.nativeElement.click();
+     }
    }
 
    ionViewDidLoad() {
